420d9352fa07763b83ca917a6dfcc80fb0b99e42
ktm000818 <kimtm0818@gmail.com>
2023-03-14 00:04:06 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import SearchAutoComplete from './components/common/SearchAutoComplete';
+import TagSearchAutoComplete from './components/common/TagSearchAutoComplete';
 
 
 function App() {
   return (
+    <>
       <h1>Hello</h1>
+      <div style={{ display: 'flex' }}>
+        <SearchAutoComplete size='large' />
+        <TagSearchAutoComplete size='large' />
+      </div>
+    </>
   );
 }
 
