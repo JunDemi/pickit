8973a415849fe26b176b2b242913df1b23c0faf8
권태준 <44458173+Gwontaejun@users.noreply.github.com>
2023-03-15 18:03:27 +0900

    Feat: 알림 퍼블리싱 작업

--- src/components/header/notify.tsx
+++ src/components/header/notify.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { usePopup } from "hooks";
 import Popup from "components/common/Popup";
+import { usePopup } from "hooks";
 
 const Notify = () => {
   const notifyPopup = usePopup();
@@ -11,13 +11,28 @@ const Notify = () => {
 
   return (
     <>
-      <button type="button" onClick={onNotifyOpen}>
-        focusTrap Outer!
+      <button type="button" className="btn-notify" onClick={onNotifyOpen}>
+        <span className="new">1</span>
       </button>
-      <Popup popupHooks={notifyPopup}>
-        <button type="button" onClick={() => notifyPopup.toggle()}>
-          focusTrap Inner!
-        </button>
+      <Popup popupHooks={notifyPopup} className="inHeader profile">
+        <div className="notify">
+          <div className="popup-header">
+            <h1>알림</h1>
+            <span>(22)</span>
+          </div>
+          <div className="popup-content">
+            <ul>
+              <li tabIndex={1}>
+                <img
+                  src={require("assets/images/DummyProfile.png")}
+                  alt="das"
+                />
+                <div>[태연] 회원님의 댓글에 답글이 달렸습니다.</div>
+                <span>45분전</span>
+              </li>
+            </ul>
+          </div>
+        </div>
       </Popup>
     </>
   );
