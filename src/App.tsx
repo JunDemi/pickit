066d35748ef718d8e6a748ee67db07902581b6b9
Gwontaejun <hope10204@daum.net>
2023-03-15 23:29:56 +0900

    Feat: 알림 팝업 퍼블리싱 완료, ReactQueryProvider 지정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
-  return <div></div>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div></div>
+    </QueryClientProvider>
+  )
 }
 
 export default App;
