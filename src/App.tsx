9568f581ef5ca19ba12cce68f19ce3f89429c727
ktm000818 <kimtm0818@gmail.com>
2023-04-10 20:18:21 +0900

    Feat: 헤더 Home -> 루트로 이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import Header from 'components/header/Header';
+import ResponsiveHeader from 'components/header/ResponsiveHeader';
 import WorldCupGame from 'components/worldcup/WorldCupGame';
-import React from 'react';
+import React, { useState } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Home, Register } from 'routes';
+import { Home, Login, Register } from 'routes';
 /* 프로필, 알림 팝업 기능 Header Component에 추가 예정 
 import { RecoilRoot } from 'recoil';
 import WorldCupGame from 'components/worldcup/WorldCupGame';
@@ -20,13 +22,17 @@ import 'assets/styles/Header.scss';
 const queryClient = new QueryClient();
 
 function App() {
+  const [showSide, setShowSide] = useState(false);
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
+        <Header showSide={showSide} setShowSide={setShowSide} />
+        <ResponsiveHeader showSide={showSide} setShowSide={setShowSide} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/game" element={<WorldCupGame />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
