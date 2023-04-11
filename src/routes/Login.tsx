9c1a3da095f65af75ac43dbe6286e0cf795792c4
ktm000818 <kimtm0818@gmail.com>
2023-04-11 15:41:23 +0900

    Feat: 로그인 컴포넌트 개발(미완)

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,5 +1,41 @@
+import React from 'react';
+import 'assets/styles/Login.scss';
+import { ReactComponent as Banner } from 'assets/images/banner.svg';
+
 function Login() {
-  return null;
+  return (
+    <div className="login-container">
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          height: '100%',
+          alignItems: 'center',
+          marginLeft: '50px',
+        }}
+      >
+        <div
+          style={{
+            width: '60%',
+            height: '70%',
+          }}
+        >
+          <Banner style={{ width: '100%', height: '100%' }} />
+        </div>
+        <div
+          style={{
+            width: '40%',
+            height: '70%',
+            border: '1px solid black',
+            marginLeft: '50px',
+            marginRight: '50px',
+          }}
+        >
+          test
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
