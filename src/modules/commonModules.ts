f5589c24ce7063d3f34166d34cd77b65f013ebfe
ktm000818 <kimtm0818@gmail.com>
2023-04-12 20:02:06 +0900

    Feat: 공통함수 모듈화

---
+++ src/modules/commonModules.ts
@@ -0,0 +1,9 @@
+export function isLogin() {
+    const { token } = JSON.parse(localStorage.getItem('recoil-persist') || '');
+
+    if (token) {
+        return true;
+    } else {
+        return false;
+    }
+}
